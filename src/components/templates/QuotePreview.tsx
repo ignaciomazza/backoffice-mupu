b8580ace45cc3f6f4fa43db34f95505792300c32
Ignacio Mazza <juanignaciomazza@hotmail.com>
2025-07-29 11:54:50 -0300

    Modififcacion momentanea

--- src/components/templates/QuotePreview.tsx
+++ src/components/templates/QuotePreview.tsx
@@ -85,7 +85,7 @@ export default function QuotePreview({ quote, user }: QuotePreviewProps) {
                     d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
                   />
                 </svg>
-                +54 9 11 5970 1234
+                +54 9 11 2301 5658
               </li>
               <li className="flex items-center gap-1 font-light">
                 <svg
